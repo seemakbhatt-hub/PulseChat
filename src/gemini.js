d7c7c206fcfeb4b459c58e292ca0f14da56f3d94
seemakbhatt-hub <seemakbhatt@gmail.com>
2026-04-25 00:57:04 +0530

    Update gemini.js

--- src/gemini.js
+++ src/gemini.js
@@ -2,6 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
+
 export const getAIResponse = async (prompt) => {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const result = await model.generateContent(prompt);
