cbf958bc0b22d12b8ec54f63ee719913c3ca2e3c
seemakbhatt-hub <seemakbhatt@gmail.com>
2026-06-04 08:08:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -16,5 +16,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-export const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
